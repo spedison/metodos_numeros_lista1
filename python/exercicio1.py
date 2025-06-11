bc78a4a389b341af8a9b9d6a5ba1a6dac42a0d45
spedison <spedison@gmail.com>
2025-06-11 06:58:30 -0300

    Professor autorizou o formato e aqui está com todos os exercícios foram resolvidos e ajustes foram realizados.

--- python/exercicio1.py
+++ python/exercicio1.py
@@ -1,36 +1,40 @@
 import math
 
+
 def calculaF(x):
-    return math.log(5+6*x)
+    return math.log(5 + 6 * x)
+
 
 def calculaP1(x):
-    return math.log(5) + x * 6. / 5.
+    return math.log(5) + (x * (6. / 5.))
+
 
 def calculaP2(p1, x):
-    return p1 - (x**2 * ( 36./50.))
+    return p1 - (math.pow(x, 2.) * (36. / 50.))
+
 
 def calculaP3(p2, x):
-    return p2 + (x**3 * (432. / 150.))
+    return p2 + (math.pow(x, 3.) * (432. / (125. * 6.)))
+
 
 def calculaP4(p3, x):
-    return p3 - (x**4 * (7776./15000.))
+    return p3 - (math.pow(x, 4.) * (7776. / (625. * 24.)))
+
 
 def executaExercicio():
-
     with open("../dados/exercicio1.csv", "w") as arquivo:
-
-        for i in range(0,25000):
-
-            val = float(i/20000.) - .5
+        for i in range(0, 25000):
+            val = float(i / 20000.) - .5
 
             valFx = calculaF(val)
             valP1 = calculaP1(val)
-            valP2 = calculaP2(valP1,val)
-            valP3 = calculaP3(valP2,val)
-            valP4 = calculaP4(valP3,val)
+            valP2 = calculaP2(valP1, val)
+            valP3 = calculaP3(valP2, val)
+            valP4 = calculaP4(valP3, val)
 
             str = f"{val};{valFx};{valP1};{valP2};{valP3};{valP4}\n"
             arquivo.write(str)
+
 
 if __name__ == '__main__':
     executaExercicio()
